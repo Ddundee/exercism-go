9d5550eda6d04352bf653ffb1028cd00568395c7
Dhanush R. Chilakala <dhanush.actual1@gmail.com>
2024-10-21 04:22:35 -0400

    logs_logs_logs

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,37 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, c := range log {
+		if c == '❗' {
+			return "recommendation"
+		} else if c == '🔍' {
+			return "search"
+		} else if c == '☀' {
+			return "weather"
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	var newLog string
+	for _, c := range log {
+		if c == oldRune {
+			newLog += string(newRune)
+		} else {
+			newLog += string(c)
+		}
+	}
+	return newLog
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
